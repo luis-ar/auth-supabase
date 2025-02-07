b93488266e61cd26452d8f14985d92cf4f52374c
Luis Santos <luisdemaryori@gmail.com>
2025-02-07 16:09:44 -0500

    mejoras 12

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@ import Data from "@/components/Data";
 import PageLogin from "@/components/formLogin";
 import Image from "next/image";
 
-
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Next.jsJJakka!</h1>
+      <h1>Welcome hello word!</h1>
       <PageLogin />
       {/* <Data /> */}
     </div>
